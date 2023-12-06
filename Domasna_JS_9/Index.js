84dba56a0e12b6c6b2943656ed995ffc59936b8c
Ljochev <83249296+Ljochev@users.noreply.github.com>
2023-12-06 00:30:20 -0600

    Homework 9

---
+++ Domasna_JS_9/Index.js
@@ -0,0 +1,173 @@
+
+// 1)_____________________________________
+
+
+class Car{
+    constructor(make, model, year, mileage){
+        this.make = make
+        this.model = model
+        this.year = year
+        this.mileage = mileage
+    
+    }
+    start(){
+        console.log("Engine started")
+    }
+    drive(distance){
+        if(distance > 0) {
+        this.mileage = this.mileage + distance
+        } else {
+            console.log("Invalid distance!")
+        }
+    }
+    getMilege() {
+        console.log(`The current milege is ${this.mileage}`)
+    }
+}
+const myCar = new Car("Mercedes", "CLS", 2022, 11043)
+myCar.start()
+myCar.getMilege()
+myCar.drive(30)
+myCar.getMilege()
+
+// 2)__________________________________________
+
+
+class Animal{
+    constructor(name, colour, sex) {
+        this.name = name
+        this.colour = colour
+        this.sex = sex
+    }
+info(){
+    console.log(`My name is ${this.name}, I'm ${this.sex} and my colour is ${this.colour}`)
+}
+name(){
+    return this.name
+}
+}
+
+class Dog extends Animal {
+    constructor(name, colour, sex, breed, age ){
+        super(name, colour, sex)
+        this.breed = breed
+        this.age = age
+        
+    }
+    myDog(){
+        console.log(`I'm ${this.age},  my colour is ${this.colour}`)
+    }
+    dogBreed(){
+        return this.breed
+    }
+}
+
+const myAnimal = new Animal("Rex", "Brown", "Male")
+myAnimal.info()
+const myDog = new Dog("Max", "Black", "Male", "Pitbull", 31)
+myDog.myDog()
+console.log(myDog.dogBreed())
+
+// 3)_________________________________________ 
+
+
+
+class Rectangle{
+    #width
+    #height
+    constructor(color, width, height){
+        this.#width = width
+    this.#height = height
+    this.color = color
+    }
+    getShapeType(){
+        return "Rectangle"
+    }
+    get getWidth(){
+return this.#width
+    }
+    get getHeight(){
+        return this.#height
+            }
+    set setWidth(newWidth) {
+        if (newWidth > 0) {
+            this.#width = newWidth
+        } else {
+            console.log("Width must be a positive number!")
+        }
+        }
+        set setHeight(newHeight) {
+            if (newHeight > 0) {
+                this.#height = newHeight
+            } else {
+                console.log("Height must be a positive number!")
+            }
+            }
+    getArea(){
+        return this.#width * this.#height
+    }
+    describe(){
+        console.log(`The rectangle's color is ${this.color}, has wight of ${this.#width} and height of ${this.#height}`)
+    }
+}
+const rectangle1 = new Rectangle("blue", 12, 13)
+console.log(rectangle1.getArea())
+rectangle1.describe()
+rectangle1.setWidth = 22
+rectangle1.setHeight = 23
+console.log(rectangle1.getWidth)
+console.log(rectangle1.getHeight)
+
+rectangle1.describe()
+
+const rectangle2 = new Rectangle("Red", 43, 23)
+rectangle2.describe()
+console.log(rectangle1.getArea())
+rectangle2.setWidth = 13
+rectangle2.setHeight = 17
+rectangle2.describe()
+console.log(" and area of ", rectangle2.getArea() )
+
+
+// 4)_________________________________________ 
+
+
+class BankAccount{
+    #balance
+    constructor(accountNumber, accountHolder, balance) {
+        this.accountNumber = accountNumber
+        this.accountHolder = accountHolder
+        this.#balance = balance
+    }
+    deposit(amount){
+        this.#balance += amount
+    }
+    withdraw(amount){
+        if(amount < this.#balance){
+            this.#balance -= amount
+        } else {
+            console.log("Ammount entered is above your current balance")
+        }
+    }
+    get getBalance(){
+        return this.#balance
+    }
+  }
+
+  const accountKosta = new BankAccount(12345678, "Kostadin Ljochev", 300)
+  console.log(accountKosta.getBalance)
+  accountKosta.deposit(50)
+  console.log(accountKosta.getBalance)
+  accountKosta.withdraw(100)
+  console.log(accountKosta.getBalance)
+  accountKosta.withdraw(300)
+  console.log(accountKosta.getBalance)
+
+  const accountMyke = new BankAccount(87654321, "Myke Bryan", 600)
+
+  accountMyke.withdraw(450)
+  console.log(accountMyke.getBalance)
+  accountMyke.deposit(50)
+  console.log(accountMyke.getBalance)
+  accountMyke.withdraw(500)
+  console.log(accountMyke.getBalance)
